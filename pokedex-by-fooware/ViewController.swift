a773181944819e930989199928afa15f440cca37
Senn XL <sennfoo@hermawans-macbook-pro.local>
2016-03-29 21:24:28 +0700

    Initial Commit

---
+++ pokedex-by-fooware/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-fooware
+//
+//  Created by Hermawan Surjadi on 3/29/16.
+//  Copyright © 2016 Fooware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
